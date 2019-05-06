9d983281d3024db246330ba3cbc20ab3d7c9e46f
Takato Kimura <4ui.iurz1@gmail.com>
2019-05-06 09:44:17 +0900

    Init

---
+++ README.md
@@ -0,0 +1,15 @@
+# PyTorch implementation of AdaCos
+This repository contains code for **AdaCos** based on [AdaCos: Adaptively Scaling Cosine Logits for Effectively Learning Deep Face Representations](https://arxiv.org/abs/1905.00292) implemented in PyTorch.
+
+## TODO
+- [ ] Train on WebFace
+
+## Requirements
+- Python 3.6
+- PyTorch 1.0
+
+## Training
+### MNIST
+```
+python mnist_train.py --metric adacos
+```
---
+++ metrics.py
@@ -0,0 +1,41 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.nn import Parameter
+import math
+
+
+class AdaCos(nn.Module):
+    def __init__(self, num_features, num_classes, m=0.50):
+        super(AdaCos, self).__init__()
+        self.num_features = num_features
+        self.n_classes = num_classes
+        self.s = math.sqrt(2) * math.log(num_classes - 1)
+        self.m = m
+        self.W = Parameter(torch.FloatTensor(num_classes, num_features))
+        nn.init.xavier_uniform_(self.W)
+
+    def forward(self, input, label):
+        # normalize features
+        x = F.normalize(input)
+        # normalize weights
+        W = F.normalize(self.W)
+        # dot product
+        logits = F.linear(x, W)
+        # add margin
+        theta = torch.acos(torch.clamp(logits, -1.0 + 1e-7, 1.0 - 1e-7))
+        target_logits = torch.cos(theta + self.m)
+        one_hot = torch.zeros_like(logits)
+        one_hot.scatter_(1, label.view(-1, 1).long(), 1)
+        output = logits * (1 - one_hot) + target_logits * one_hot
+        # feature re-scale
+        with torch.no_grad():
+            B_avg = torch.where(one_hot < 1, self.s * torch.exp(logits), torch.zeros_like(logits))
+            B_avg = torch.sum(B_avg) / input.size(0)
+            # print(B_avg)
+            theta_med = torch.median(theta)
+            self.s = torch.log(B_avg) / torch.cos(torch.min(math.pi/4 * torch.ones_like(theta_med), theta_med))
+        # print(self.s)
+        output *= self.s
+
+        return output
